a1390229d2ff974201913487f9a73d4c95d43900
James Lamb <jaylamb20@gmail.com>
2018-11-30 16:39:45 -0600

    updated gitignore and added demo script

---
+++ demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# failure is a natural part of life
+set -e
+
+for pkg in xgboost argparse; do
+
+    # The R package
+    doppel-describe \
+        -p ${pkg} \
+        --language R \
+        --data-dir $(pwd)/test_data
+
+    # The python package
+    doppel-describe \
+        -p ${pkg} \
+        --language python \
+        --data-dir $(pwd)/test_data
+
+    # test
+    doppel-test \
+        --files $(pwd)/test_data/python_${pkg}.json,$(pwd)/test_data/r_${pkg}.json \
+        | tee ${pkg}.log \
+        | cat
+
+done
