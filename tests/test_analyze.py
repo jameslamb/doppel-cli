9fb68ccff89c562a33fc7bbe3b77ef07ed08b0c6
Bedhead015 <bedhead015@apollo.Home>
2018-12-10 13:22:37 -0700

    added analyze.py test

---
+++ tests/test_analyze.py
@@ -0,0 +1,16 @@
+import unittest
+import analyze as al
+
+
+class AnalyzeTest(unittest.TestCase):
+    
+   	#Test for _log_info and should print msg
+	def test_log_info(self):
+
+		self.assertEqual(al._log_info("Hello World!"), "Hello World", "Printed the msg")
+
+
+	#Test for _get_arg_names and should return arg names.
+	#def test_get_arg_names(self):
+
+		#self.assertEqual(al._get_arg_names(), )
