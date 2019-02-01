4024732edfe2ad3c2c6fcbde7e66d4a607ae793e
James Lamb <jaylamb20@gmail.com>
2019-01-31 20:25:43 -0600

    more changes to travis

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -10,11 +10,10 @@ wget ${MINICONDA_INSTALLER} -O miniconda.sh;
 bash miniconda.sh -b -p ${CONDA_DIR}
 export PATH="${CONDA_DIR}/bin:$PATH"
 
-alias conda=${CONDA_DIR}/bin/conda
 hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
+${CONDA_DIR}/bin/conda config --set always_yes yes --set changeps1 no
+${CONDA_DIR}/bin/conda update -q conda
+${CONDA_DIR}/bin/conda info -a
 
 # Set up R (gulp)
 ${CONDA_DIR}/bin/conda install -c r \
