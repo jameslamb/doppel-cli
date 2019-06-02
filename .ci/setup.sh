65cc9426ca87fcf5b6b94420466f3222bde7920b
James Lamb <jaylamb20@gmail.com>
2019-06-01 20:33:59 -0500

    updated argparse version

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -18,13 +18,12 @@ ${CONDA_DIR}/bin/conda info -a
 # Set up R (gulp)
 ${CONDA_DIR}/bin/conda install -c r \
     r-base \
-    r-argparse \
     r-jsonlite \
     r-lintr \
     r-r6
 
 # Get packages for testing
-${CONDA_DIR}/bin/Rscript -e "install.packages('futile.logger', repos = '${CRAN_MIRROR}')"
+${CONDA_DIR}/bin/Rscript -e "install.packages(c('futile.logger', 'argparse'), repos = '${CRAN_MIRROR}')"
 ${CONDA_DIR}/bin/pip install \
     --user \
     argparse \
