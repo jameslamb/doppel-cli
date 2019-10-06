7ed31d55254bcc9f9fcd8cfdd2af4e76d81b8080
Lorena Freitas <lorenafreitas@Lorenas-MacBook-Pro.local>
2019-10-06 14:34:47 +0200

    Deleted commented line with roxygen2 second installation

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -34,9 +34,6 @@ ${CONDA_DIR}/bin/conda install -c conda-forge \
 
 export PATH=${PATH}:${CONDA_DIR}/bin
 
-# Get R packages for testing
-#${CONDA_DIR}/bin/Rscript -e "install.packages(c('roxygen2'), repos = '${CRAN_MIRROR}')"
-
 # Get Python packages for testing
 ${CONDA_DIR}/bin/pip install \
     --user \
