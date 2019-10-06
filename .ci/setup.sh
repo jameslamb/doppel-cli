05278d3f0d42de3a52748d930e4c1a223f7827d5
Lorena Freitas <lorenafreitas@Lorenas-MacBook-Pro.local>
2019-10-06 13:37:28 +0200

    Removed duplicate R installations from setup.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -33,7 +33,7 @@ ${CONDA_DIR}/bin/conda install -c conda-forge \
 export PATH=${PATH}:${CONDA_DIR}/bin
 
 # Get R packages for testing
-${CONDA_DIR}/bin/Rscript -e "install.packages(c('argparse', 'covr', 'futile.logger', 'roxygen2'), repos = '${CRAN_MIRROR}')"
+#${CONDA_DIR}/bin/Rscript -e "install.packages(c('argparse', 'covr', 'futile.logger', 'roxygen2'), repos = '${CRAN_MIRROR}')"
 
 # Get Python packages for testing
 ${CONDA_DIR}/bin/pip install \
