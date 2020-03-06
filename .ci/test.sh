e42a3a001c8145c94884112280e5de5ebbfcfaa2
James Lamb <jaylamb20@gmail.com>
2020-03-06 01:00:34 -0600

    [ci] added tests on analyze.py handling of Python built-ins

--- .ci/test.sh
+++ .ci/test.sh
@@ -14,7 +14,7 @@ CI_TOOLS=$(pwd)/.ci
 # Test coverage stuff
 MIN_UNIT_TEST_COVERAGE=95
 MIN_ANALYZE_R_TEST_COVERAGE=100
-MIN_ANALYZE_PY_TEST_COVERAGE=90
+MIN_ANALYZE_PY_TEST_COVERAGE=97
 
 # Make sure we're living in conda land
 export PATH="$HOME/miniconda/bin:$PATH"
--- integration_tests/test-packages/python/pythonspecific/pythonspecific/mod_one/__init__.py
+++ integration_tests/test-packages/python/pythonspecific/pythonspecific/mod_one/__init__.py
@@ -2,3 +2,5 @@
 from .some_function import some_function
 from .SomeClass import SomeClass
 from .SomeClass import SOME_CONSTANT
+from .wrap_min import wrap_min
+from .wrap_min import MinWrapper
